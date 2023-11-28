d8638af3dd85ba2b48190e79d7f598378477227a
Irfan Ghat <58285388+projectfinalaudio@users.noreply.github.com>
2023-11-28 18:25:31 +0300

    11/28/23: State

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![allow(non_snake_case)]
+#![allow(unused_imports)]
 
 use chrono::{DateTime, Utc};
 use chrono::serde::ts_seconds;
@@ -54,22 +55,25 @@ fn main() {
 }
 
 fn App(cx: Scope) -> Element {
-    render! {
-        StoryListing {
-            story: StoryItem {
-                id: 0,
-                title: "hello hackernews".to_string(),
-                url: None,
-                text: None,
-                by: "Author".to_string(),
-                score: 0,
-                descendants: 0,
-                time: chrono::Utc::now(),
-                kids: vec![],
-                r#type: "".to_string(),
+    cx.render(rsx! {
+        div {
+            display: "flex",
+            flex_direction: "row",
+            align_items: "center",
+            width: "100%",
+            background: "#eeee",
+            border_radius: "10px",
+            box_shadow: "0 1rem 1rem 0 rgba(0, 0, 0, .2)",
+            div {
+                width: "50%",
+                Stories {}
+            }
+            div {
+                width: "50%",
+                Preview {}
             }
         }
-    }
+    })
 }
 
 #[inline_props]
@@ -81,6 +85,7 @@ fn StoryListing(cx: Scope, story: StoryItem) -> Element {
         score,
         time,
         kids,
+        id,
         ..
     } = story;
 
@@ -89,7 +94,6 @@ fn StoryListing(cx: Scope, story: StoryItem) -> Element {
         .trim_start_matches("https://")
         .trim_start_matches("http://")
         .trim_start_matches("www.");
-
     let score = format!("{score} {}", if *score == 1 { " point" } else { " points" });
     let comments = format!(
         "{} {}",
@@ -100,15 +104,14 @@ fn StoryListing(cx: Scope, story: StoryItem) -> Element {
             " comments"
         }
     );
-
     let time = time.format("%D %l:%M %p");
 
     cx.render(rsx! {
         div {
-           padding: ".7rem",
+            padding: "0.5rem",
             position: "relative",
             div {
-                font_size: "1.7rem",
+                font_size: "1.5rem",
                 a {
                     href: url,
                     "{title}"
@@ -128,14 +131,99 @@ fn StoryListing(cx: Scope, story: StoryItem) -> Element {
                     "{score}"
                 }
                 div {
-                    padding_left: ".5rem",
+                    padding_left: "0.5rem",
                     "by {by}"
                 }
                 div {
-                    padding_left: ".5rem",
+                    padding_left: "0.5rem",
+                    "{time}"
+                }
+                div {
+                    padding_left: "0.5rem",
                     "{comments}"
                 }
             }
         }
     })
+}
+
+fn Stories(cx: Scope) -> Element {
+    render! {
+        StoryListing {
+            story: StoryItem {
+                id: 0,
+                title: "hello hacker news".to_string(),
+                url: None,
+                text: None,
+                by: "Author".to_string(),
+                score: 0,
+                descendants: 0,
+                time: chrono::Utc::now(),
+                kids: vec![],
+                r#type: "".to_string(),
+            }
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+enum PreviewState {
+    Unset,
+    Loading,
+    Loaded(StoryPageData),
+}
+
+
+fn Preview(cx: Scope) -> Element {
+    let preview_state = PreviewState::Unset;
+    match preview_state {
+        PreviewState::Unset => render! {
+            "Hover over a story to preview it here..."
+        },
+        PreviewState::Loading => render! {
+            "Loading..."
+        },
+        PreviewState::Loaded(story) => {
+            let title = &story.item.title;
+            let url = story.item.url.as_deref().unwrap_or_default();
+            let text = story.item.text.as_deref().unwrap_or_default();
+            render! {
+                div {
+                    padding: ".5rem",
+                    div {
+                        font_size: "1.5rem",
+                        a {
+                            href: "{url}",
+                            "{title}"
+                        }
+                    }
+                    div {
+                        dangerous_inner_html: "{text}"
+                    }
+                    for comment in &story.comments {
+                        Comment { comment: comment.clone() }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[inline_props]
+fn Comment(cx: Scope, comment: Comment) -> Element<'a> {
+    render! {
+        div {
+            padding: ".5rem",
+            div {
+                color: "gray",
+                "by {comment.by}"
+            }
+            div {
+                dangerous_inner_html: "{comment.text}"
+            }
+            for kid in &comment.sub_comments {
+                Comment { comment: kid.clone() }
+            }
+        }
+    }
 }
